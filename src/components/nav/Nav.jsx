866eac2304d33acebfdd62dfe09d6bbab6078161
youngjerald239 <youngjerald239@gmail.com>
2022-08-28 17:35:10 -0500

    added icons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './nav.css'
-import {FaHome} from 'react-icons/fa'
+import {FaHome, FaUserAstronaut} from 'react-icons/fa'
+import {MdLaptopChromebook, MdOutlineContactMail} from 'react-icons/md'
+import {RiServiceFill} from 'react-icons/ri'
+import {AiOutlineFundProjectionScreen} from 'react-icons/ai'
 
 const Nav = () => {
   return (
     <nav>
       <a href="#"><FaHome/></a>
+      <a href="#about"><FaUserAstronaut/></a>
+      <a href="#experience"><MdLaptopChromebook/></a>
+      <a href="#services"><RiServiceFill/></a>
+      <a href="#contact"><MdOutlineContactMail/></a>
+      <a href="#portfolio"><AiOutlineFundProjectionScreen/></a>
     </nav>
   )
 }
