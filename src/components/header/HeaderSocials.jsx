ce07547e17c55a4b204b33a316474d998030f01e
youngjerald239 <youngjerald239@gmail.com>
2022-08-28 16:22:37 -0500

    added social icons to page

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
+import {FaItchIo, FaTwitterSquare} from 'react-icons/fa'
 
 const HeaderSocials = () => {
   return (
     <div className="header_socials">
         <a href="https://www.linkedin.com/in/jerald-young-232aa720a/" target="blank"><BsLinkedin/></a>
         <a href="https://github.com/youngjerald239" target="blank"><BsGithub/></a>
-        <a href="https://youngjerald.itch.io/" target="blank"></a>
+        <a href="https://twitter.com/JeraldYoung19" target="blank"><FaTwitterSquare/></a>
+        <a href="https://youngjerald.itch.io/" target="blank"><FaItchIo/></a>
     </div>
   )
 }
