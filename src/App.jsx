3fba9a4e218393393298f4455b85255a15c1287d
youngjerald239 <youngjerald239@gmail.com>
2022-08-28 14:44:38 -0500

    added initial styling components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import './App.css';
 import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Contact from './components/contact/Contact'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Footer from './components/footer/Footer'
+import Testimonial from './components/testimonial/Testimonial'
 
-function App() {
+const App = () => {
   return (
-    < >
+    <>
       <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
     </>
   );
 }
