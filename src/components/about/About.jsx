451e180f13d8aac01abc11b7a32fd56dfcc852d7
youngjerald239 <youngjerald239@gmail.com>
2022-08-28 19:23:55 -0500

    added class about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assets/me-about.png'
-import {GiAchievement} from 'react-icons/gi'
+import {GiAchievement, GiGameConsole} from 'react-icons/gi'
+import {FaDev} from 'react-icons/fa'
 
 const About = () => {
   return (
@@ -11,7 +12,7 @@ const About = () => {
       <div className="container about_container">
         <div className="about_me">
           <div className="about_me-image">
-            <img src={ME} alt="About Image" />
+            <img src={ME} alt="about-me"/>
           </div>
         </div>
         <div className="about_content">
@@ -19,17 +20,17 @@ const About = () => {
             <article className='about_card'>
               <GiAchievement className='about_icon'/>
               <h5>Experience</h5>
-              <small>1.5 years Fullstack Developing</small>
+              <small>1.5 years Fullstack Developing. Customer Service. Technical Support. 24-hour Banker experience. Smartphone troubleshooting experience. Home-Security device Troubleshooting(Vivint) Billing(smartphone,banking,home-security) SalesForce tools customer service experience</small>
             </article>
             <article className='about_card'>
-              <GiAchievement className='about_icon'/>
-              <h5>Experience</h5>
-              <small>1.5 years Fullstack Developing</small>
+              <FaDev className='about_icon'/>
+              <h5>DevTools</h5>
+              <small>Html(Intermediate) JavaScript(Intermediate) CSS(Intermediate) React(Intermediate) Python(Junior) Ruby(Junior) TailWind(Junior) NextJs(Junior)</small>
             </article>
             <article className='about_card'>
-              <GiAchievement className='about_icon'/>
-              <h5>Experience</h5>
-              <small>1.5 years Fullstack Developing</small>
+              <GiGameConsole className='about_icon'/>
+              <h5>Video Game Developer</h5>
+              <small>Pygame(Intermediate) Unity(Beginner) Unreal(Junior) Godot(Beginner)</small>
             </article>
           </div>
         </div>
