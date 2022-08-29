e0176bbab875ab0c536ba6c04ac28ce3da6f33d4
youngjerald239 <youngjerald239@gmail.com>
2022-08-28 22:14:19 -0500

    added skills

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,135 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheck} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>My Skills</h5>
+      <h2>Acquired Experience</h2>
+
+      <div className="container experience_container">
+        <div className="experience_frontend">
+            <h3>Frontend Development</h3>
+            <div className="experience_content">
+              <article className="experience_details">
+                <BsPatchCheck className="experience_details-icon"/>
+                <div>
+                  <h4>HTML</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+              <article className="experience_details">
+                <BsPatchCheck className="experience_details-icon"/>
+                <div>
+                  <h4>CSS</h4>
+                  <small className="text-light">Intermediate</small>
+                  </div>
+              </article>
+              <article className="experience_details">
+                <BsPatchCheck className="experience_details-icon"/>
+                <div>
+                  <h4>JavaScript</h4>
+                  <small className="text-light">Intermediate</small>
+                  </div>
+              </article>
+              <article className="experience_details">
+                <BsPatchCheck className="experience_details-icon"/>
+                <div>
+                  <h4>Bootsrap</h4>
+                  <small className="text-light">Experienced</small>
+                  </div>
+              </article>
+              <article className="experience_details">
+                <BsPatchCheck className="experience_details-icon"/>
+                <div>
+                  <h4>TailWind</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
+              <article className="experience_details">
+                <BsPatchCheck className="experience_details-icon"/>
+                <div>
+                  <h4>React</h4>
+                  <small className="text-light">Experienced</small>
+                  </div>
+              </article>
+              <article className="experience_details">
+                <BsPatchCheck className="experience_details-icon"/>
+                <div>
+                  <h4>NextJs</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
+              <article className="experience_details">
+                <BsPatchCheck className="experience_details-icon"/>
+                <div>
+                  <h4>ReactNative</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
+            </div>
+        </div>
+
+        <div className="experience_backend">
+            <h3>Backend Development</h3>
+             <div className="experience_content">
+              <article className="experience_details">
+                <BsPatchCheck className="experience_details-icon"/>
+                <div>
+                  <h4>Nodejs</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
+              <article className="experience_details">
+                <BsPatchCheck className="experience_details-icon"/>
+                <div>
+                  <h4>MongoDB</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
+              <article className="experience_details">
+                <BsPatchCheck className="experience_details-icon"/>
+                <div>
+                  <h4>Postman</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+              <article className="experience_details">
+                <BsPatchCheck className="experience_details-icon"/>
+                <div>
+                  <h4>Ruby</h4>
+                  <small className="text-light">Intermediate</small>
+                </div>
+              </article>
+              <article className="experience_details">
+                <BsPatchCheck className="experience_details-icon"/>
+                <div>
+                  <h4>MySQL</h4>
+                  <small className="text-light">Basic</small>
+                </div>
+              </article>
+              <article className="experience_details">
+                <BsPatchCheck className="experience_details-icon"/>
+                <div>
+                  <h4>Python</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+              <article className="experience_details">
+                <BsPatchCheck className="experience_details-icon"/>
+                <div>
+                  <h4>Sanatra</h4>
+                  <small className="text-light">Basic</small>
+                </div>
+              </article>
+            </div>
+        </div>
+        <div className="experience_gamedev">
+
+        </div>
+      </div>
+    </section>
   )
 }
 
