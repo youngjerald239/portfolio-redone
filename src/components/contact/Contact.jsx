a3340932d21a31c744e3a14bea1f4d10d59826a1
youngjerald239 <youngjerald239@gmail.com>
2022-08-29 17:08:59 -0500

    created contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineMailOutline} from 'react-icons/md'
+import {BsLinkedin, BsWhatsapp, BsTwitter} from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+          <div className="contact_options">
+             <article className="contact_option">
+                <MdOutlineMailOutline/>
+                <h4>Email</h4>
+                <h5>youngjerald239@gmail.com</h5>
+                <a href="mailto:youngjerald239@gmail.com" target="_blank">Send an Email</a>
+             </article>
+             <article className="contact_option">
+                <BsLinkedin/>
+                <h4>LinkedIn Message</h4>
+                <h5>youngjerald239@gmail.com</h5>
+                <a href="https://www.linkedin.com/in/jerald-young-232aa720a/" target="_blank">Connect with LinkedIn</a>
+             </article>
+             <article className="contact_option">
+                <BsWhatsapp/>
+                <h4>WhatsApp</h4>
+                <h5>Phone Messenger</h5>
+                <a href="https://api.whatsapp.com/send?phone=18032717767" target="_blank">Send a message</a>
+             </article>
+             <article className="contact_option">
+                <BsTwitter/>
+                <h4>Twitter</h4>
+                <h5>https://twitter.com/messages</h5>
+                <a href="https://twitter.com/messages/compose?recipient_id={JeraldYoung19}" target="_blank">Private Twitter message</a>
+             </article>
+          </div>
+          <form action="">
+             <input type="text" name='name' placeholder='Your Full Name' required/>
+             <input type="email" name='email' placeholder='Email Address' required/>
+             <textarea name="message" rows="7" placeholder='Enter your message here' required></textarea>
+             <button type="submit" className='btn btn-primary'>Send Message</button>
+          </form>
+       </div>
+    </section>
   )
 }
 
