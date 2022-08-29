122c8054bd6b208abde374728228ef6896e149af
youngjerald239 <youngjerald239@gmail.com>
2022-08-29 13:15:55 -0500

    added project info and images

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.png'
+import IMG3 from '../../assets/portfolio3.png'
+import IMG4 from '../../assets/portfolio4.png'
+import IMG5 from '../../assets/portfolio5.png'
 
 const Portfolio = () => {
   return (
@@ -7,10 +12,48 @@ const Portfolio = () => {
     <h5>My FullStack Portfolio</h5>
     <h3>Recent Work</h3>
 
-    <div className="container portfolio_container"></div>
-    <h3>This is a portfolio item title</h3>
-    <a href="https://github.com/youngjerald239" className="btn" target='_blank'>Github</a>
-    <a href="https://github.com/youngjerald239" className="btn-primary" target='_blank'>Live Demo</a>
+    <div className="container portfolio_container">
+      <article className="portfolio_container">
+        <div className="portfolio_image">
+        <img src={IMG1} alt=""/>
+        </div>
+        <h3>This is a portfolio item title</h3>
+        <a href="https://github.com/youngjerald239/Weather_App" className="btn" target="_blank" rel="noreferrer">Github</a>
+        <a href="https://jweather-app.netlify.app/" className="btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
+      </article>
+      <article className="portfolio_container">
+        <div className="portfolio_image">
+        <img src={IMG1} alt=""/>
+        </div>
+        <h3>This is a portfolio item title</h3>
+        <a href="https://github.com/youngjerald239/Weather_App" className="btn" target="_blank" rel="noreferrer">Github</a>
+        <a href="https://jweather-app.netlify.app/" className="btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
+      </article>
+      <article className="portfolio_container">
+        <div className="portfolio_image">
+        <img src={IMG1} alt=""/>
+        </div>
+        <h3>This is a portfolio item title</h3>
+        <a href="https://github.com/youngjerald239/Weather_App" className="btn" target="_blank" rel="noreferrer">Github</a>
+        <a href="https://jweather-app.netlify.app/" className="btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
+      </article>
+      <article className="portfolio_container">
+        <div className="portfolio_image">
+        <img src={IMG1} alt=""/>
+        </div>
+        <h3>This is a portfolio item title</h3>
+        <a href="https://github.com/youngjerald239/Weather_App" className="btn" target="_blank" rel="noreferrer">Github</a>
+        <a href="https://jweather-app.netlify.app/" className="btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
+      </article>
+      <article className="portfolio_container">
+        <div className="portfolio_image">
+        <img src={IMG1} alt=""/>
+        </div>
+        <h3>This is a portfolio item title</h3>
+        <a href="https://github.com/youngjerald239/Weather_App" className="btn" target="_blank" rel="noreferrer">Github</a>
+        <a href="https://jweather-app.netlify.app/" className="btn-primary" target="_blank" rel="noreferrer">Live Demo</a>
+      </article>
+    </div>
     </section>
   )
 }
