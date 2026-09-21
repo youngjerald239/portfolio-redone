916ee93fe1a7a34556fe80ab554b9788f2e2eef1
Jerald Young <youngjerald239@gmail.com>
2026-09-21 17:50:20 -0400

    Updated portfolio css
    Add a new background and dark and light mode

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -10,13 +10,13 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState('#home')
   return (
     <nav>
-  <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}><FaHome/></a>
-      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserAstronaut/></a>
-      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdLaptopChromebook/></a>
-      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineFundProjectionScreen/></a>
-      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdAnimation/></a>
-      <a href="#testimonial" onClick={() => setActiveNav('#testimonial')} className={activeNav === '#testimonial' ? 'active' : ''}><GiGameConsole/></a>
-      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdOutlineContactMail/></a>
+  <a href="#home" title="Home" aria-label="Home" onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}><FaHome/></a>
+  <a href="#about" title="About me" aria-label="About me" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserAstronaut/></a>
+  <a href="#experience" title="Skills and experience" aria-label="Skills and experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><MdLaptopChromebook/></a>
+  <a href="#portfolio" title="Web projects" aria-label="Web projects" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineFundProjectionScreen/></a>
+  <a href="#services" title="Character animations" aria-label="Character animations" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdAnimation/></a>
+  <a href="#testimonial" title="Games" aria-label="Games" onClick={() => setActiveNav('#testimonial')} className={activeNav === '#testimonial' ? 'active' : ''}><GiGameConsole/></a>
+  <a href="#contact" title="Contact" aria-label="Contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdOutlineContactMail/></a>
       
     </nav>
   )
